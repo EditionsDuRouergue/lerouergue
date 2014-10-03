57ff645ccd3be3f2341c4748ebff77e7b69ea616
Christophe Espiau <christophe.espiau@korora.fr>
2014-10-03 18:40:25 +0200

    Rouergue theme updated: template.php file updated and javascript file added to handle display of Univers block menu.

---
+++ profiles/rouergue/themes/custom/rouergue_bootstrap/js/universes_navigation.js
@@ -0,0 +1,14 @@
+(function ($) {
+  Drupal.behaviors.UniversesNavigation =  {
+    attach: function(context, settings) {
+  	  $('#block-menu-menu-universe ul').removeClass('nav');
+//  	  $('#block-menu-menu-universe ul').attr('id','menu-universe');
+//  	  $('#block-menu-menu-universe ul li ul li ul').addClass('collapse');
+//  	  $('#block-menu-menu-universe ul li ul').addClass('collapse');
+//  	  $('#block-menu-menu-universe ul').attr('data-toggle','collapse');
+  	  
+  	  
+
+    }
+  };
+})(jQuery);
--- profiles/rouergue/themes/custom/rouergue_bootstrap/template.php
+++ profiles/rouergue/themes/custom/rouergue_bootstrap/template.php
@@ -4,3 +4,15 @@
  * @file
  * template.php
  */
+
+// function rouergue_bootstrap_menu_local_action($variables) {
+// 	return theme_menu_local_action($variables);
+// }
+
+function rouergue_bootstrap_menu_link($variables) {
+	return theme_menu_link($variables);
+}
+
+if (!drupal_is_front_page()) {
+	drupal_add_js(drupal_get_path('theme', 'rouergue_bootstrap') .'/js/universes_navigation.js', array('type' => 'file', 'scope' => 'footer'));
+}
