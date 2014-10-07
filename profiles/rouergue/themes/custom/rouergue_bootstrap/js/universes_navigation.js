5094f049211772a4fee651621dad323449450bb5
Christophe Espiau <christophe.espiau@korora.fr>
2014-10-07 17:05:14 +0200

    Code cleaning in custom theme's universes_navigation.js file.

--- profiles/rouergue/themes/custom/rouergue_bootstrap/js/universes_navigation.js
+++ profiles/rouergue/themes/custom/rouergue_bootstrap/js/universes_navigation.js
@@ -2,13 +2,6 @@
   Drupal.behaviors.UniversesNavigation =  {
     attach: function(context, settings) {
   	  $('#block-menu-menu-universe ul').removeClass('nav');
-//  	  $('#block-menu-menu-universe ul').attr('id','menu-universe');
-//  	  $('#block-menu-menu-universe ul li ul li ul').addClass('collapse');
-//  	  $('#block-menu-menu-universe ul li ul').addClass('collapse');
-//  	  $('#block-menu-menu-universe ul').attr('data-toggle','collapse');
-  	  
-  	  
-
     }
   };
 })(jQuery);
