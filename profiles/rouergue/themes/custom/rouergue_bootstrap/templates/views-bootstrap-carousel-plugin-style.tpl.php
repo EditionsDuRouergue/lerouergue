4991fabefa41258a889eba18ead67929679ed3e0
Christophe Espiau <christophe.espiau@korora.fr>
2014-10-20 20:12:46 +0200

    Feat. Catalog updated: 1) Nodequeue 'new_books' created 2) View displays 'catalog_new_books_page' and 'catalog_new_books_block' created to implement 'Nouveautés' block. 3) template file views-bootstrap-carousel-plugin-style.tpl.php created in theme's template folder to implement carousel with 6 items per slide.

---
+++ profiles/rouergue/themes/custom/rouergue_bootstrap/templates/views-bootstrap-carousel-plugin-style.tpl.php
@@ -0,0 +1,37 @@
+<div id="views-bootstrap-carousel-<?php print $id ?>" class="col-sm-12 <?php print $classes ?>" <?php print $attributes ?>>
+	<!-- The array containing rows is splitted into X, X being the number of books we want per slide -->
+	<?php $slides = array_chunk($rows, 6, true); ?>
+	
+  <?php if ($indicators): ?>
+    <!-- Carousel indicators -->
+    <ol class="carousel-indicators">
+      <?php foreach ($slides as $key => $value): ?>
+        <li data-target="#views-bootstrap-carousel-<?php print $id ?>" data-slide-to="<?php print $key ?>" class="<?php if ($key === 0) print 'active' ?>"></li>
+      <?php endforeach ?>
+    </ol>
+  <?php endif ?>
+
+  <!-- Carousel items - Here, a carousel item (slide) contains several view items (row)-->
+  <div class="carousel-inner">
+  <?php foreach ($slides as $key => $slide): ?>
+	  <div class="item <?php if ($key === 0) print 'active' ?>">
+			    <?php foreach ($slide as $key => $item): ?>
+			    	<div class="col-sm-2">
+			      	<?php print $item ?>
+			      </div>
+			    <?php endforeach ?>
+	  </div>
+  <?php endforeach ?>
+   
+  </div>
+
+  <?php if ($navigation): ?>
+    <!-- Carousel navigation -->
+    <a class="carousel-control left" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="prev">
+      <span class="icon-prev"></span>
+    </a>
+    <a class="carousel-control right" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="next">
+      <span class="icon-next"></span>
+    </a>
+  <?php endif ?>
+</div>
