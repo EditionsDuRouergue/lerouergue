473cdd149797e8c3af8a684277c7f0a019a5ee34
Christophe Espiau <christophe.espiau@korora.fr>
2014-11-03 16:54:13 +0100

    Rouergue theme updated: new suggestion function implemented to override rendering of fied_inner_pages and make it a bootstrap modal dialog.

--- profiles/rouergue/themes/custom/rouergue_bootstrap/template.php
+++ profiles/rouergue/themes/custom/rouergue_bootstrap/template.php
@@ -16,3 +16,52 @@ function rouergue_bootstrap_menu_link($variables) {
 if (!drupal_is_front_page()) {
 	drupal_add_js(drupal_get_path('theme', 'rouergue_bootstrap') .'/js/universes_navigation.js', array('type' => 'file', 'scope' => 'footer'));
 }
+
+function rouergue_bootstrap_field__field_inner_pages($variables) {
+	//dpm($variables);
+	
+
+	/*
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.	
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+  */
+
+	$output .= '<div class="modal fade modal-lg" id="rouergue-pdf-viewer" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">';
+		$output .= '<div class="modal-dialog">';
+			$output .= '<div class="modal-content">';
+				$output .= '<div class="modal-header">';
+					$output .= '<button type="button" class="close" data-dismiss="modal">';
+						$output .= '<span aria-hidden="true">&times;</span><span class="sr-only">Fermer</span>';
+					$output .= '</button>';
+					$output .= '<h4 class="modal-title" id="ModalLabel">Pages intérieures</h4>';
+				$output .= '</div>';
+				$output .= '<div class="modal-body">';
+					$output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+				  foreach ($variables['items'] as $delta => $item) {
+				    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+				    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+				  }
+				  $output .= '</div>';
+				$output .= '</div>';
+				$output .= '<div class="modal-footer">';
+					$output .= '<button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>';
+				$output .= '</div>';
+			$output .= '</div>';
+		$output .= '</div>';
+	$output .= '</div>';
+
+  return $output;
+}
