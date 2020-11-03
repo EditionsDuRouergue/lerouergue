bed1f00734e97a5161e4e04f4b091c0ce5d96833
ACTES-SUD\f.challier <f.challier@actes-sud.fr>
2020-11-03 14:29:14 +0100

    DownGrade Webform

--- profiles/rouergue/modules/contrib/webform/templates/webform-confirmation.tpl.php
+++ profiles/rouergue/modules/contrib/webform/templates/webform-confirmation.tpl.php
@@ -11,15 +11,10 @@
  *
  * Available variables:
  * - $node: The node object for this webform.
- * - $progressbar: The progress bar 100% filled (if configured). This may not
- *   print out anything if a progress bar is not enabled for this node.
- * - $confirmation_message: The confirmation message input by the webform
- *   author.
+ * - $confirmation_message: The confirmation message input by the webform author.
  * - $sid: The unique submission ID of this submission.
- * - $url: The URL of the form (or for in-block confirmations, the same page).
  */
 ?>
-<?php print $progressbar; ?>
 
 <div class="webform-confirmation">
   <?php if ($confirmation_message): ?>
@@ -30,5 +25,5 @@
 </div>
 
 <div class="links">
-  <a href="<?php print $url; ?>"><?php print t('Go back to the form'); ?></a>
+  <a href="<?php print url('node/' . $node->nid) ?>"><?php print t('Go back to the form') ?></a>
 </div>
